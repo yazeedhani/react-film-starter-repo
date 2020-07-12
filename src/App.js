86d31853801e3c4d8a733c3410e11b83b07ed76f
009kings <009kings@gmail.com>
2020-07-12 16:32:57 -0700

    step 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilmDetails from './FilmDetails';
+import FilmListing from './FilmListing';
 
 class App extends Component {
   render() {
     return (
       <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
+        <FilmListing />
+        <FilmDetails />
       </div>
     );
   }
---
+++ src/FilmListing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilmListing extends Component {
+  render() {
+    return (
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+      </div>
+    );
+  }
+}
+
+export default FilmListing;
