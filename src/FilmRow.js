d87651d13371c86fa4f4cd5bbde859e27d7a427f
009kings <009kings@gmail.com>
2020-07-12 16:47:01 -0700

    step 7

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -4,7 +4,14 @@ class FilmRow extends Component {
   render() {
     return (
       <div className="film-row">
-        <h1>{this.props.film.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} 
+              alt={`Poster of the film ${this.props.film.title}`} 
+        />
+
+        <div className="film-summary">
+          <h1>{this.props.film.title}</h1>
+          <p>{this.props.film.release_date.substr(0,4)}</p>
+        </div>
       </div>
     );
   }
