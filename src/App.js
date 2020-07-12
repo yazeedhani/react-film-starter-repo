50c202b6244a5a68d09b5a130f0e750145ab22f4
009kings <009kings@gmail.com>
2020-07-12 16:27:08 -0700

    step 1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="film-library">
+        <div className="film-list">
+          <h1 className="section-title">FILMS</h1>
+        </div>
+
+        <div className="film-details">
+          <h1 className="section-title">DETAILS</h1>
+        </div>
       </div>
     );
   }
