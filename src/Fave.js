f57c16d1894d84752528e71832deee549d5bcb80
009kings <009kings@gmail.com>
2020-07-12 20:06:03 -0700

    task 4

--- src/Fave.js
+++ src/Fave.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
 class Fave extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isFave: false,
+    }
+  }
+
   handleClick = (e) => {
     e.stopPropagation()
-		console.log("handling fave click!");
+    console.log("handling fave click!");
+    this.setState({isFave: !this.state.isFave})
 	}
 
 
   render() {
+    let icon = this.state.isFave ? 'remove_from_queue' : 'add_to_queue'
+
     return (
-      <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
-        <p className="material-icons">add_to_queue</p>
+      <div className={`film-row-fave ${icon}`} onClick={this.handleClick}>
+        <p className="material-icons">{icon}</p>
       </div>
     );
   }
