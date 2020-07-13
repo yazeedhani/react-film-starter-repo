7c5983d5d6e83ac534bf680fa3c4ae57a2e345c0
009kings <009kings@gmail.com>
2020-07-12 19:21:07 -0700

    step 3

--- src/Fave.js
+++ src/Fave.js
@@ -8,7 +8,7 @@ class Fave extends Component {
 
   render() {
     return (
-      <div className="film-row-fave add_to_queue">
+      <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
         <p className="material-icons">add_to_queue</p>
       </div>
     );
