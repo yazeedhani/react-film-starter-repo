b5fda01cc31047d11bc082454a68178d05b8131a
009kings <009kings@gmail.com>
2020-07-12 19:51:07 -0700

    step 3.1 and 3.2

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -3,9 +3,13 @@ import FilmPoster from './FilmPoster';
 import Fave from './Fave';
 
 class FilmRow extends Component {
+  handleDetailsClick = film => {
+    console.log(`Fetching movie details for ${film.title}`)
+  }
+
   render() {
     return (
-      <div className="film-row">
+      <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film)}>
         <FilmPoster 
           poster_path={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`}
           title={this.props.film.title}
