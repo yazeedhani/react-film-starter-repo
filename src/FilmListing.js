2d52c3c7cd57c85d74cd9bd8c144be12535b45be
009kings <009kings@gmail.com>
2020-07-12 19:36:06 -0700

    step 2.2

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -10,10 +10,21 @@ class FilmListing extends Component {
     const allFilms = this.props.films.map((film, i) => <FilmRow film={film} key={`filmRow-${i}`} />)
 
     return (
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-          {allFilms}
-        </div>
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+        <div className="film-list-filters">
+          <div className="film-list-filter">
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+          </div>
+          <div className="film-list-filter">
+            FAVES
+            <span className="section-count">0</span>
+          </div>
+      </div>
+  
+      {allFilms}
+    </div>
     );
   }
 }
