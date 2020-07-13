d4e1d27d40cc125de86bae498ff8fcf4b933333a
009kings <009kings@gmail.com>
2020-07-12 20:10:33 -0700

    task 5

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmListing extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filter: 'all',
+    }
+  }
+
   handleFilterClick = filter => {
     console.log(`Setting filter to ${filter}`)
+    this.setState({filter})
   }
 
   render() {
@@ -13,11 +21,11 @@ class FilmListing extends Component {
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
         <div className="film-list-filters">
-          <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+          <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('all')}>
             ALL
             <span className="section-count">{this.props.films.length}</span>
           </div>
-          <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+          <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('faves')}>
             FAVES
             <span className="section-count">0</span>
           </div>
