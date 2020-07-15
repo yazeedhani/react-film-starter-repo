09e7713b017d61a06ec73f0e006a79d9d7319cc4
009kings <009kings@gmail.com>
2020-07-15 14:40:43 -0700

    Actually p1 complete

---
+++ src/FilmPoster.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FilmPoster extends Component {
+  render() {
+    return (
+      <img 
+        src={this.props.src} 
+        alt={this.props.alt} 
+      />
+    )
+  }
+}
+
+export default FilmPoster;
--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import FilmPoster from './FilmPoster';
 
 class FilmRow extends Component {
   render() {
     return (
       <div className="film-row">
-        <img src={`https://images.tmdb.org/t/p/w780${this.props.film.poster_path}`} alt={`Poster for the film ${this.props.film.title}`} />
+        <FilmPoster 
+          src={`https://images.tmdb.org/t/p/w780${this.props.film.poster_path}`}
+          alt={`Poster for the film ${this.props.film.title}`}
+        />
 
         <div className="film-summary">
           <h1>{this.props.film.title}</h1>
