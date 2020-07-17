92533a7f5baf6413f1ef8858bc67b8b5a81461e4
009kings <009kings@gmail.com>
2020-07-17 09:20:32 -0700

    1.1 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FilmDetails from './FilmDetails';
 import FilmListing from './FilmListing';
 import TMDB from './TMDB';
 
 function App() {
+  const [films, setFilms] = useState(TMDB.films);
+  const [current, setCurrent] = useState({});
+
   return (
     <div className="film-library">
       <FilmListing films={TMDB.films} />
