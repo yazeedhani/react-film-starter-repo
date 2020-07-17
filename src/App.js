5895740a70b9b7fc87d74a4f4af454c63b6e07d4
009kings <009kings@gmail.com>
2020-07-17 13:57:02 -0700

    1.1 and 1.2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import TMDB from './TMDB';
 function App() {
   const [films, setFilms] = useState(TMDB.films);
   const [current, setCurrent] = useState({});
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
 
+  console.log(popularFilmsUrl)
+  
   const handleDetailsClick = film => {
     console.log(`Fetching movie details for ${film.title}`)
     setCurrent(film)
