833790673a718ca0b4367a26de697ff7c6cf5248
009kings <009kings@gmail.com>
2020-07-17 09:28:25 -0700

    2.1, 2.2, 2.3

--- src/Fave.js
+++ src/Fave.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 function Fave(props) {
-  const [isFave, setIsFave] = useState(false);
   const [icon, setIcon] = useState('add_to_queue')
 
   useEffect(()=>{
-    setIcon(isFave ? 'remove_from_queue' : 'add_to_queue')
-  }, [isFave])
+    setIcon(props.isFave ? 'remove_from_queue' : 'add_to_queue')
+  }, [props.isFave])
 
   const handleClick = (e) => {
     e.stopPropagation()
     console.log("handling fave click!");
-    setIsFave(!isFave)
+    props.onFaveToggle()
   }
-
   
   return (
     <div className={`film-row-fave ${icon}`} onClick={handleClick}>
